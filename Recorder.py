8dfbaddf58c0e76b25d5c99ef07ab6500887db34
Hrvoje Matić <ibicom@ibicom.hr>
2019-07-14 17:12:47 +0200

    Added Post-recording algorithm
    Missing frames repair process is designed, so that the previous (up to)
    5 frames are duplicate from the current frame.
    Given there is a possibility that only a few frames can be missed, the
    value of 5 frames ensures us that the algorithm won't copy more that 5
    frames from the starting frame (which should be the first frame of
    recording process).

--- Recorder.py
+++ Recorder.py
@@ -3,6 +3,8 @@ import time
 import multiprocessing as mp
 import cv2
 import os
+import subprocess
+import platform
 
 from Shared.Configuration import Configuration
 from Recorder.VideoRecorder import VideoRecorder
@@ -25,26 +27,26 @@ def start_single_camera(camera_number, address, path, fps, scheduled_end_of_reco
     video.record()
 
 
-if __name__ == "__main__":
+def run_main():
     config = Configuration()
     eor = time.time() + int(config.common["playtime"])
     video_addresses = str(config.recorder["video"]).split(",")
-    rootPath = os.path.normpath(r"{}".format(config.recorder["path"]))
+    recording_path = os.path.normpath(r"{}".format(config.recorder["recording-path"]))
 
-    if not os.path.isdir(rootPath):
-        os.mkdir(rootPath)
+    if not os.path.isdir(recording_path):
+        os.mkdir(recording_path)
 
     processes = []
     i = 0
     for v in video_addresses:
         i += 1
         processes.append(mp.Process(target=start_single_camera,
-                         args=(i,
-                               v,
-                               os.path.normpath(r"{}/{}".format(config.recorder["path"], i)),
-                               int(config.recorder["fps"]),
-                               eor,
-                               config)))
+                                    args=(i,
+                                          v,
+                                          os.path.normpath(r"{}/{}".format(recording_path, i)),
+                                          int(config.recorder["fps"]),
+                                          eor,
+                                          config)))
 
     for p in processes:
         p.start()
@@ -55,3 +57,5 @@ if __name__ == "__main__":
     cv2.destroyAllWindows()
 
 
+if __name__ == "__main__":
+    run_main()
