fb7f2e499925f4b8bac12067bc0ad9f3e8ca0cf4
Hrvoje Matić <ibicom@ibicom.hr>
2019-07-15 12:09:55 +0200

    Fixed Missing Files Algorithm
    Created a separate thread that takes care of fixing missing frames, by
    using the previous frame.
    Removed the algorithm from CapturedFrame threads.

--- Recorder/CapturedFrame.py
+++ Recorder/CapturedFrame.py
@@ -1,44 +1,17 @@
 import cv2
 import threading
-import os
-import shutil
 
 
 class CapturedFrame(object):
-    def __init__(self, frame, file_path, camera_number, fps, second, frame_number, target_path):
+    def __init__(self, frame, file_path):
         self.frame = frame
         self.filePath = file_path
-        self.fps = fps
-        self.camera_number = camera_number
-        self.second = second
-        self.frame_number = frame_number
-        self.target_path = target_path
-        self.missing_shots = 0
 
     def save_file_async(self):
         single_thread = threading.Thread(target=self.save_file, args=())
         single_thread.start()
         return single_thread
 
-    def ensure_previous_file_exist(self, given_second, given_frame_number):
-        expected_second = given_second
-        expected_frame_number = given_frame_number - 1
-
-        if expected_frame_number == 0:
-            expected_second = given_second - 1
-            expected_frame_number = self.fps
-
-        expected_file = os.path.normpath(
-                    r"{target_path}/camera_{camera_number}_frame_{currentTime}_{frameNumber}.png"
-                    .format(target_path=self.target_path,
-                            camera_number=self.camera_number,
-                            currentTime=int(expected_second),
-                            frameNumber=str(expected_frame_number).zfill(4)))
-        if not os.path.isfile(expected_file) and self.missing_shots <= 5:
-            self.missing_shots += 1
-            shutil.copyfile(self.filePath, expected_file)
-            self.ensure_previous_file_exist(expected_second, expected_frame_number)
-
     def save_file(self):
-        cv2.imwrite(self.filePath, self.frame)
-        self.ensure_previous_file_exist(self.second, self.frame_number)
+        image = cv2.UMat(self.frame)
+        cv2.imwrite(self.filePath, image)
--- Recorder/VideoRecorder.py
+++ Recorder/VideoRecorder.py
@@ -3,12 +3,13 @@ import queue
 import threading
 import time
 import logging
-
+import shutil
 import cv2
 
 from Recorder.CapturedFrame import CapturedFrame
 from Recorder.VideoCaptureAsync import VideoCaptureAsync
 from Shared.LogHandler import LogHandler
+
 
 class VideoRecorder(object):
     def __init__(self, camera_number, video_source, target_path, fps,
@@ -32,9 +33,20 @@ class VideoRecorder(object):
         self.logger = LogHandler("recording")
         self.logger.info('Camera {}, on playground {} has started recording.'.format(camera_number, playground))
 
+        self.read_lock = threading.Lock()
+
+        self.firstFrame = None
+        self.repairing_frame = None
+        self.lastRecordedFrame = None
+
         self.frameProcessingStop = False
         self.frameProcessingThread = threading.Thread(target=self.process_frames, args=())
         self.frameProcessingThread.start()
+
+        self.frameCleanupStop = False
+        self.frameCleanupThread = threading.Thread(target=self.cleanup, args=())
+        self.frameCleanupThread.start()
+
         self.capture = VideoCaptureAsync(self.video_source,
                                          self.frameSize[0],
                                          self.frameSize[1],
@@ -50,12 +62,16 @@ class VideoRecorder(object):
         for t in self.frameProcessingThreads:
             t.join()
 
-    def read_frame(self):
+    def read_frame(self, current_time):
         try:
             ret, image = self.capture.read()
             if ret:
-                current_time = time.time()
                 frame_number = int(round((((current_time - int(current_time)) / 100) * self.fps) * 100, 1)) + 1
+
+                if self.firstFrame is None:
+                    self.firstFrame = (int(current_time), frame_number)
+
+                self.lastRecordedFrame = (int(current_time), frame_number)
 
                 file_path = os.path.normpath(
                     r"{target_path}/camera_{camera_number}_frame_{currentTime}_{frameNumber}.png"
@@ -65,13 +81,7 @@ class VideoRecorder(object):
                             frameNumber=str(frame_number).zfill(4)))
 
             if not self.frameQueue.full():
-                self.frameQueue.put_nowait(CapturedFrame(image,
-                                                         file_path,
-                                                         self.camera_number,
-                                                         self.fps,
-                                                         int(current_time),
-                                                         frame_number,
-                                                         self.target_path))
+                self.frameQueue.put_nowait(CapturedFrame(image, file_path))
         except Exception as ex:
             self.logger.error("Camera {}, on playground {} is not responding."
                               .format(self.camera_number, self.playground))
@@ -86,11 +96,15 @@ class VideoRecorder(object):
                 self.capture.start()
 
                 while time.time() < self.scheduled_end_of_recording and self.casting:
-                    time_elapsed = time.time() - previous_snapshot
+                    current_time = time.time()
+                    current_snapshot = int(current_time) + ((int(round((((current_time - int(current_time)) / 100) *
+                                                                        self.fps) * 100, 1)) + 1) / 10000)
 
-                    if time_elapsed >= (1. / self.fps):
-                        previous_snapshot = time.time()
-                        read_thread = threading.Thread(target=self.read_frame, args=())
+                    if current_snapshot != previous_snapshot:
+                        previous_snapshot = int(current_time) + \
+                                            ((int(round((((current_time - int(current_time)) / 100) * self.fps) *
+                                                        100, 1)) + 1) / 10000)
+                        read_thread = threading.Thread(target=self.read_frame, args=(time.time(),))
                         read_thread.start()
             except Exception as ex:
                 self.logger.error("Camera {}, on playground {} is not responding."
@@ -104,6 +118,9 @@ class VideoRecorder(object):
             self.frameProcessingStop = True
             self.frameProcessingThread.join()
             self.casting = False
+            self.frameProcessingStop = True
+            self.frameCleanupStop = True
+            self.frameCleanupThread.join()
 
     # Finishes the video recording therefore the thread too
     def stop(self):
@@ -114,9 +131,67 @@ class VideoRecorder(object):
         else:
             pass
 
+        self.frameCleanupStop = True
+        self.frameCleanupThread.join()
+
         self.logger.warning('Recording with camera {}, on playground {} has been forcibly interrupted.'
-                        .format(self.camera_number, self.playground))
+                            .format(self.camera_number, self.playground))
+
+    def cleanup(self):
+
+        # Wait until first frame is captured
+        while not self.frameCleanupStop:
+            if self.firstFrame is not None:
+                break
+
+        # Wait 3 more seconds (to allow all threads to complete work)
+        time.sleep(3)
+
+        # Every second raise repair algorithm for the one second period (iterations = fps)
+        try:
+            if self.repairing_frame is None:
+                self.repairing_frame = self.firstFrame
+
+            while (not self.frameCleanupStop) or \
+                    self.lastRecordedFrame[0] > self.repairing_frame[0] or \
+                    (self.lastRecordedFrame[0] == self.repairing_frame[0] and
+                     self.lastRecordedFrame[1] + 1 > self.repairing_frame[1]):
+
+                # Get the next frame that needs to be checked
+                previous_frame = self.repairing_frame
+                self.repairing_frame = (self.repairing_frame[0] + 1, 1) \
+                    if self.repairing_frame[1] == 31 \
+                    else (self.repairing_frame[0], self.repairing_frame[1] + 1)
+
+                # Repair next (fps) number of frames
+                for i in range(1, self.fps + 1):
+                    previous_file = os.path.normpath(
+                        r"{target_path}/camera_{camera_number}_frame_{currentTime}_{frameNumber}.png"
+                        .format(target_path=self.target_path,
+                                camera_number=self.camera_number,
+                                currentTime=previous_frame[0],
+                                frameNumber=str(previous_frame[1]).zfill(4)))
+                    expected_file = os.path.normpath(
+                        r"{target_path}/camera_{camera_number}_frame_{currentTime}_{frameNumber}.png"
+                        .format(target_path=self.target_path,
+                                camera_number=self.camera_number,
+                                currentTime=self.repairing_frame[0],
+                                frameNumber=str(self.repairing_frame[1]).zfill(4)))
+
+                    with self.read_lock:
+                        if not os.path.isfile(expected_file):
+                            shutil.copyfile(previous_file, expected_file)
+                        else:
+                            break
+
+                # Wait for the next iteration
+                current_time = time.time()
+                while int(round((((current_time - int(current_time)) / 100) * self.fps) * 100, 1)) + 1 != 1:
+                    current_time = time.time()
+        except Exception as ex:
+            self.logger.error("Error fixing missing frames."
+                              .format(self.camera_number, self.playground))
 
     def cv2error(self):
         self.logger.error("Camera {}, on playground {} is not responding."
-                      .format(self.camera_number, self.playground))
+                          .format(self.camera_number, self.playground))
