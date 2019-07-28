e229f9e333c2892552bb46718d6e9294497ca4b9
Hrvoje Matić <ibicom@ibicom.hr>
2019-07-28 23:27:43 +0200

    Reduced AI detector latency
    - added try-catch block to Detector
    - added logger to Detector
    - ensured ThreadPoolExecutor tasks to complet both in VideoRecorder, and Detector
    - Removed async function for saving json in CapturedFrame

--- ActivityDetector/Detector.py
+++ ActivityDetector/Detector.py
@@ -1,17 +1,19 @@
 import os
 import queue
 import threading
-
+import concurrent.futures
 import cv2
 import imutils
 
 from ActivityDetector.AiModelConfig import AiModelConfig
 from Mask_RCNN.mrcnn import model as modellib
 from Shared.Configuration import Configuration
+from Shared.LogHandler import LogHandler
 
 
 class Detector(object):
-    def __init__(self, ai_queues):
+    def __init__(self, playground: int, ai_queues: list):
+        self.playground = playground
         self.ai_queues = ai_queues
         self.output_queue = queue.Queue(maxsize=10000)
         self.stop_detection = False
@@ -20,6 +22,10 @@ class Detector(object):
         self.config = Configuration().activity_detector
         self.class_names = self.get_class_names()
         self.sports_ball_id = self.class_names.index("sports ball")
+
+        # Logger
+        self.logger = LogHandler("detector")
+        self.logger.info('Detector on playground {} has ras started detection.'.format(playground))
 
         self.detection_thread = None
         self.start_detection()
@@ -35,70 +41,77 @@ class Detector(object):
         number_of_queues = len(self.ai_queues)
         first_frames_retrieved = 0
         queues_currently_empty = number_of_queues
+        ai_tasks = []
 
-        while not self.stop_detection:
-            for index, ai_queue in enumerate(self.ai_queues):
-                try:
-                    captured_frame = ai_queue.get(block=False)
-                    # poison pill found; aborting detection
-                    if captured_frame is None:
-                        self.stop_detection = True
-                        break
+        with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+            try:
+                while not self.stop_detection:
+                    for index, ai_queue in enumerate(self.ai_queues):
+                        try:
+                            captured_frame = ai_queue.get(block=False)
+                            # poison pill found; aborting detection
+                            if captured_frame is None:
+                                self.stop_detection = True
+                                break
 
-                    if queue_index_to_is_first_frame_retrieved[index] is False:
-                        first_frames_retrieved += 1
-                        queue_index_to_is_first_frame_retrieved[index] = True
-                    if queue_index_to_is_queue_empty[index] is True:
-                        queue_index_to_is_queue_empty[index] = False
-                        queues_currently_empty -= 1
+                            if queue_index_to_is_first_frame_retrieved[index] is False:
+                                first_frames_retrieved += 1
+                                queue_index_to_is_first_frame_retrieved[index] = True
+                            if queue_index_to_is_queue_empty[index] is True:
+                                queue_index_to_is_queue_empty[index] = False
+                                queues_currently_empty -= 1
 
-                    image = captured_frame.frame
-                    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                    image = imutils.resize(image, width=512)
+                            image = captured_frame.frame
+                            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                            image = imutils.resize(image, width=512)
 
-                except Exception as ex:
-                    print("Unsuccessful in retrieving image from ai_queue: " + str(ex))
-                    if ai_queue.empty():
-                        queue_index_to_is_queue_empty[index] = True
-                        queues_currently_empty += 1
+                        except Exception as ex:
+                            print("Unsuccessful in retrieving image from ai_queue: " + str(ex))
+                            if ai_queue.empty():
+                                queue_index_to_is_queue_empty[index] = True
+                                queues_currently_empty += 1
 
-                        if first_frames_retrieved == number_of_queues and queues_currently_empty == number_of_queues:
-                            print("All queues are empty! Stopping detection.")
-                            self.stop_detection = True
-                            break
-                    continue
+                                if first_frames_retrieved == number_of_queues and \
+                                        queues_currently_empty == number_of_queues:
+                                    print("All queues are empty! Stopping detection.")
+                                    self.stop_detection = True
+                                    break
+                            continue
 
-                result = model.detect([image], verbose=1)[0]
+                        result = model.detect([image], verbose=1)[0]
 
-                output_json_array = []
+                        output_json_array = []
 
-                for i in range(0, len(result["scores"])):
-                    # extract the bounding box information, class ID and predicted probability
-                    class_id = result["class_ids"][i]
-                    if class_id == self.sports_ball_id:
-                        (startY, startX, endY, endX) = result["rois"][i]
-                        score = result["scores"][i]
+                        for i in range(0, len(result["scores"])):
+                            # extract the bounding box information, class ID and predicted probability
+                            class_id = result["class_ids"][i]
+                            if class_id == self.sports_ball_id:
+                                (startY, startX, endY, endX) = result["rois"][i]
+                                score = result["scores"][i]
 
-                        output_json = {
-                            "x0": int(startX),
-                            "y0": int(startY),
-                            "x1": int(endX),
-                            "y1": int(endY),
-                            "score": int(score)
-                        }
+                                output_json = {
+                                    "x0": int(startX),
+                                    "y0": int(startY),
+                                    "x1": int(endX),
+                                    "y1": int(endY),
+                                    "score": int(score)
+                                }
 
-                        output_json_array.append(output_json)
+                                output_json_array.append(output_json)
 
-                balls_identified = len(output_json_array)
+                        balls_identified = len(output_json_array)
 
-                if balls_identified > 0:
-                    if balls_identified > 1:
-                        print("[!] More than one ball identified in frame!")
-                    captured_frame.json = output_json_array
-                    self.output_threads.append(captured_frame.save_json())
-
-        for thread in self.output_threads:
-            thread.join()
+                        if balls_identified > 0:
+                            if balls_identified > 1:
+                                print("[!] More than one ball identified in frame!")
+                            captured_frame.json = output_json_array
+                            ai_task = executor.submit(captured_frame.save_json)
+                            ai_tasks.append(ai_task)
+            except:
+                self.stop_detection = True
+                self.logger.error('Detector on playground {} is in error state.'.format(self.playground))
+            finally:
+                concurrent.futures.wait(fs=ai_tasks, return_when="ALL_COMPLETED")
 
     def start_detection(self):
         self.detection_thread = threading.Thread(target=self.detect, args=())
